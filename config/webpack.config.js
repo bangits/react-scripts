27cd10ddca05d9e838025dd01ab58b34f0c69c54
Armen Mesropyan <amesropyan@bangits.com>
2022-02-17 15:31:19 +0400

    Updated the aliases

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -311,7 +311,7 @@ module.exports = function (webpackEnv) {
     },
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        "@": paths.appSrc,
       },
       // This allows you to set a fallback for where webpack should look for modules.
       // We placed these paths second because we want `node_modules` to "win"
